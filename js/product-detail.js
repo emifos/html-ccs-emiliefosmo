b9eb5fa617c145e462303c87b7aea32128cc7052
emifos <emilie.fosmo@hotmail.com>
2025-09-04 07:47:30 +0200

    .

--- js/product-detail.js
+++ js/product-detail.js
@@ -52,3 +52,39 @@ async function fetchAndCreateProduct() {
 }
 
 fetchAndCreateProduct()
+
+
+const genderFilter = document.querySelector("#genderFilter");
+
+async function createFilterOptions() {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    const products = data.data;
+
+    // Hent alle kjønn fra produktene
+    const genders = products.map(product => product.gender); // EKS: "men" eller "women"
+
+    // Finn unike verdier
+    const uniqueGenders = [...new Set(genders)];
+
+    // Legg til en "All" først
+    const allOption = document.createElement("option");
+    allOption.value = "all";
+    allOption.textContent = "All";
+    genderFilter.appendChild(allOption);
+
+    // Lag <option> for hver unik gender
+    uniqueGenders.forEach(gender => {
+      const option = document.createElement("option");
+      option.value = gender;
+      option.textContent = gender.charAt(0).toUpperCase() + gender.slice(1); // f.eks. "Men"
+      genderFilter.appendChild(option);
+    });
+
+  } catch (error) {
+    console.error("Kunne ikke hente filter-data", error);
+  }
+}
+
+createFilterOptions();
